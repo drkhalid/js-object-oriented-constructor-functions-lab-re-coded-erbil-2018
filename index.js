bddcb3546e1a5f3f4f42d3c5f1adf9043d176f90
khalid jamal <kh.jamal95@gmail.com>
2018-07-29 07:58:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function Scooter(year,color,method){
+function Scooter(year, color, method){
   this.year=year
   this.color =color
   this.method=method
 }
 
-let samson = new Scooter(2015, "red", "swoosh")
+
